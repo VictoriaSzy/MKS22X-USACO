0c8ca50cc217ab03e4a65e51602a184974aeb794
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-05 18:32:29 -0500

    Added frames for methods for bronze and silver problems

--- USACO.java
+++ USACO.java
@@ -1,3 +1,10 @@
 public class USACO{
+  // Bronze level problem
+  public static int bronze(String filename) {
 
+  }
+  // Silver level problem
+  public static int silver(String filename) {
+    
+  }
 }
