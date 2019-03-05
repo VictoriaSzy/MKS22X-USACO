d7b88e412777070d94554f69a6daba18c6d75e6e
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-05 18:44:13 -0500

    Added reading in files for both bronze and silver problems

--- USACO.java
+++ USACO.java
@@ -4,10 +4,14 @@ import java.io.* ;
 public class USACO{
   // Bronze level problem
   public static int bronze(String filename) throws FileNotFoundException {
-
+    File f = new File(filename) ;
+    Scanner s = new Scanner(f) ;
+    return 0 ;
   }
   // Silver level problem
   public static int silver(String filename) throws FileNotFoundException {
-
+    File f = new File(filename) ;
+    Scanner s = new Scanner(f) ;
+    return 0 ;
   }
 }
