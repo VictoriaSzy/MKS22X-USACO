e346d4759a00be00be32008ff95913e318d80a98
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-05 18:42:22 -0500

    Added throwing FileNotFoundException

--- USACO.java
+++ USACO.java
@@ -1,10 +1,13 @@
+import java.util.* ;
+import java.io.* ;
+
 public class USACO{
   // Bronze level problem
-  public static int bronze(String filename) {
+  public static int bronze(String filename) throws FileNotFoundException {
 
   }
   // Silver level problem
-  public static int silver(String filename) {
-    
+  public static int silver(String filename) throws FileNotFoundException {
+
   }
 }
