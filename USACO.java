54b382cf54e5a0ade90f65a36a38627659fae329
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-06 11:35:13 -0500

    Continued working on bronze problem - added placing dimensions of field into own array

--- USACO.java
+++ USACO.java
@@ -3,9 +3,32 @@ import java.io.* ;
 
 public class USACO{
   // Bronze level problem
+  private int[][] field ;
   public static int bronze(String filename) throws FileNotFoundException {
     File f = new File(filename) ;
     Scanner s = new Scanner(f) ;
+    int len = 0 ;
+    int w = s.length() ;
+    String line ;
+    int[] firstRow ;
+    int[][] stompingDirections ;
+    // this first while loop helps me determine
+    for (int i = 0 ; s.hasNextLine() ; i++) {
+      if (i == 0) {
+        firstRow = line.split(" ") ;
+      }
+      if (firstRow.length > 0 && i >= firstRow[0] + 1) {
+        // we have finished scanning through the field and now we can read the stomping instructions
+
+      }
+      line = s.nextLine() ;
+    }
+    field = new int[firstRow[0]][firstRow[1]] ;
+    ////////////////////////////////////////////////////
+    Scanner ss = new Scanner(f) ;
+    while (s.hasNextLine()) {
+
+    }
     return 0 ;
   }
   // Silver level problem
