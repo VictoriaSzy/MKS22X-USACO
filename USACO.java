29e3416f85cb5802e57dd5b0443a45dbeac600b2
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-10 13:14:34 -0400

    Finished Silver problem and made update helper method to update field

--- USACO.java
+++ USACO.java
@@ -16,6 +16,7 @@ public class USACO {
       System.out.println("We couldn't find the file! UH OH") ;
     } catch (Exception e) {
       System.out.println("There's an exception being thrown but it's not caused by a missing file!") ;
+      System.out.println("Here is the specific exception: \n" + e) ;
     }
     System.out.println("**********************SILVER**********************") ;
     System.out.println("We are now testing to see how the silver problem is going!") ;
@@ -37,6 +38,9 @@ public class USACO {
     }
     int numRows = firstRow[0] ;
     int numCols = firstRow[1] ;
+    if (numRows < 3 || numRows > 100 || numCols < 3 || numCols > 100) {
+      throw new IllegalArgumentException("The number of rows and/or columns is not valid!") ;
+    }
     int[][] field = new int[numRows][numCols] ;
     String line ;
     //adding the values to the 2D array
@@ -95,7 +99,11 @@ public class USACO {
     return res ;
   }
 
-  // Silver level problem
+  /* Silver level problem
+  Given a map with '.'s for open pasture space
+  and '*' for trees,
+  calculate # of possible ways
+  to travel from (R1, C1) to (R2, C2) in T seconds*/
   public static int silver(String filename) throws FileNotFoundException {
     File f = new File(filename) ;
     Scanner s = new Scanner(f) ;
@@ -106,12 +114,20 @@ public class USACO {
     }
     int numRows = firstRow[0] ;
     int numCols = firstRow[1] ;
+    if (numRows < 2 || numRows > 100 || numCols < 2 || numCols > 100) {
+      throw new IllegalArgumentException("The number of rows and/or columns is not valid!") ;
+    }
     int time = firstRow[2] ;
+    if (time < 0 || time > 15) {
+      throw new IllegalArgumentException("The time is greater than 15 seconds or less than 0, making it invalid!") ;
+    }
     int[][] field = new int[numRows][numCols] ;
     String line ;
     for (int r = 0 ; r < numRows ; r++) {
       for (int c = 0 ; c < numCols ; c++) {
-      	field[r][c] = Integer.parseInt(s.next()) ;
+        line = s.nextLine() ;
+        if (line.charAt(c) == '.') field[r][c] = 0 ;
+        if (line.charAt(c) == '*') field[r][c] = -1 ;
       }
     }
     int[] lastRow = new int[4] ;
@@ -122,6 +138,37 @@ public class USACO {
     int startingCol = lastRow[1] ;
     int finalRow = lastRow[2] ;
     int finalCol = lastRow[3] ;
-    return 0 ;
+    // finished finding out basic info
+    field[startingRow][startingCol] = 1 ; //starting spot
+    for (int i = 0 ; i < time ; i++) {
+      field = update(field) ; //updating the field
+    }
+    return field[finalRow][finalCol] ;
+  }
+  public static int[][] update(int[][] field) {
+    // this helps update the field
+    int[][] moves = { {0,1}, {0,-1}, {1,0}, {-1,0} } ;
+    int[][] newField = new int[field.length][field[0].length] ;
+    for (int r = 0 ; r < field.length ; r++) {
+      for (int c = 0 ; c < field[0].length ; c++) {
+        if (field[r][c] != -1) {
+          int sum = 0 ;
+          for (int i = 0 ; i < 4 ; i ++) {
+            int tempR = r + moves[i][0] ;
+            int tempC = c + moves[i][1] ;
+            if (tempR < field.length && tempR > -1 && tempC < field[0].length && tempC > -1 && field[tempR][tempC] != -1) {
+              // if it's in bounds, then we can add it to the sum
+              sum += field[tempR][tempC] ;
+            }
+          }
+          newField[r][c] = sum ;
+        }
+        else {
+          // we also make the new field have -1 at this spot
+          newField[r][c] = -1;
+        }
+      }
+    }
+    return newField ;
   }
 }
